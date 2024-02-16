b27e06a569fbbee4c865f932d904f959c41d35b9
Yuxing Lu <yxlu0613@gmail.com>
2024-02-16 14:35:28 +0800

    update html

--- source/conf.py
+++ source/conf.py
@@ -21,7 +21,7 @@
 # source_parsers = {
 #     '.md': CommonMarkParser,
 # }
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
 source_suffix = ['.rst', '.md']
 extensions = [
     'recommonmark',
